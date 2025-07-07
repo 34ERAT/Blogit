343942a6192e658be446f4660dda2e3bd60bea04
34ERAT <philipkanuti@gmail.com>
2025-07-07 19:35:38 +0300

    export services using indext.ts
    update naming of  service funtion by prefixing with the
    name of the service

--- backend/src/services/blog.service.ts
+++ backend/src/services/blog.service.ts
@@ -1,17 +1,17 @@
 import { Blog } from "@prisma/client";
 import DBconnection from "../util/DBconnection";
-export async function update({ userId, id, ...update }: Blog) {
+export async function updateBlog({ userId, id, ...update }: Blog) {
   return await DBconnection.blog.update({
     where: { id, AND: { userId } },
     data: update,
   });
 }
-export async function create(blog: Blog) {
+export async function createBlog(blog: Blog) {
   return await DBconnection.blog.create({
     data: blog,
   });
 }
-export async function get(id: string) {
+export async function getBlog(id: string) {
   return await DBconnection.blog.findUnique({
     where: { id, AND: { isDeleted: false } },
   });
--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,16 +1,16 @@
 import { User } from "@prisma/client";
 import DBconnection from "../util/DBconnection";
-export async function update({ id, ...update }: User) {
+export async function updateUser({ id, ...update }: User) {
   return await DBconnection.user.update({
     where: { id },
     data: update,
   });
 }
-export async function create(user: User) {
+export async function createUser(user: User) {
   return await DBconnection.user.create({
     data: user,
   });
 }
-export async function get(id: string) {
+export async function getUser(id: string) {
   return await DBconnection.user.findUnique({ where: { id } });
 }
