9e113693f7c3c50b218de934f71559a0a895b806
34ERAT <philipkanuti@gmail.com>
2025-07-09 15:45:28 +0300

    create jwt verification middleware

---
+++ backend/src/middleware/verifyToken.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export default function async(req: Request, res: Response, next: NextFunction) {
+  const {
+    accessToken: { token },
+  } = req.cookies;
+  if (!token) {
+    res.status(400).json({ message: "not token provided" });
+  }
+  const { id } = jwt.verify(
+    token,
+    process.env.JWTSECRET as string,
+  ) as JwtPayload;
+  req.body.userId = id;
+  next();
+}
