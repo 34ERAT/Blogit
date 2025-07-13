2eb1e138132e0293935862344cc1b51588dd26bb
34ERAT <philipkanuti@gmail.com>
2025-07-14 01:22:27 +0300

    set the base url for deployment

--- frontend/src/config/axiosInstance.ts
+++ frontend/src/config/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://blogit-ei78.onrender.com/api",
   withCredentials: true,
 });
 export default axiosInstance;
